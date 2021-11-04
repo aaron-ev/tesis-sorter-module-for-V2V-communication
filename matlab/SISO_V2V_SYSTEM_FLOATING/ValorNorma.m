6f2c187e3597d876a0073d4baf5c0c079d683b5d
Aaron Escoboza Villegas <aaron.villegas@hotmail.com>
2021-11-03 17:23:07 -0700

    update sentences in objective and hypothesis

--- matlab/SISO_V2V_SYSTEM_FLOATING/ValorNorma.m
+++
@@ -1,8 +0,0 @@
-function Vnorma = ValorNorma(Q)
-val = 0;
-for i=1:length(Q)
-    val = val + imag(Q(i))*imag(Q(i)) + real(Q(i))*real(Q(i));
-end
-
-Vnorma = val;
-end
--- matlab/distance_l2.m
+++
@@ -1,7 +0,0 @@
-function Dl2 = distance_l2(yn,l)
-    % global variables
-    global M, global R,global x; 
-    for i = 1:M
-        Dl2(i) = norm(y - R(l,l).*x(:,i))^2;
-    end
-end
